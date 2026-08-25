084c824b6d9162aa7a492d8e8d212ab87ce45fbe
Jackbt21 <93138082+Jackbt21@users.noreply.github.com>
2026-08-25 14:19:20 +0100

    Update files.py
    Adds in * to correct copy_amset_files. Currently looks for transport.json which dosent exist - has been patched to look for transport_*.json* which is then renamed to transport.prev.json so it can be checked for convergence as intended

--- src/atomate2/amset/files.py
+++ src/atomate2/amset/files.py
@@ -55,17 +55,18 @@ def copy_amset_files(
         "band_structure_data.json",
         "wavefunction.h5",
         "deformation.h5",
-        "transport.json",
     ):
         found_file = get_zfile(directory_listing, file, allow_missing=True)
         if found_file is not None:
             files.append(found_file)
+    files.append("transport_*.json*")
 
     copy_files(
         src_dir,
         src_host=src_host,
         include_files=files,
         file_client=file_client,
+        allow_missing=True,
     )
 
     gunzip_files(
@@ -74,7 +75,9 @@ def copy_amset_files(
         file_client=file_client,
     )
 
-    rename_files({"transport.json": "transport.prev.json"}, allow_missing=True)
+    local_transport = next(Path().glob("transport_*.json*"), None)
+    if local_transport is not None:
+        local_transport.rename("transport.prev.json")
     logger.info("Finished copying inputs")
 
 
